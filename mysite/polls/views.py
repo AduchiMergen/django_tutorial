efed49d31864039bb17f10bf2c20fd5ed2a2c6b3
Erik Beran <eberan@gmail.com>
2012-12-09 22:47:15 -0800

    Using helper function in poll detail view dealing with 404.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render_to_response
-from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 
 def index(request):
@@ -7,10 +6,7 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
