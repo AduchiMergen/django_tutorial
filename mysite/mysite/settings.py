038066e4337cffc403bf45159232f9e985683f36
Erik Beran <eberan@gmail.com>
2012-12-09 16:12:11 -0800

    Added base_site admin template.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,7 +1,10 @@
 # Django settings for mysite project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+SITE_ROOT = os.getcwd()
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -106,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    SITE_ROOT + '/mytemplates',
 )
 
 INSTALLED_APPS = (
